11ead42fa9f781339a1743693eed952b80c101df
Michael Fellinger <michael.fellinger@xing.com>
2018-07-21 19:16:13 +0200

    Show Builds ordered by mtime

--- route_builds_project.go
+++ route_builds_project.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"os"
 	"path/filepath"
+	"sort"
 	"strings"
+	"time"
 
 	macaron "gopkg.in/macaron.v1"
 )
@@ -17,8 +20,13 @@ func getBuildsProject(ctx *macaron.Context) {
 		projectBuilds[i] = projectBuild{
 			Project: parts[1],
 			ID:      parts[2],
+			ModTime: modTime(projectID),
 		}
 	}
+
+	sort.SliceStable(projectBuilds, func(i int, j int) bool {
+		return projectBuilds[i].ModTime.After(projectBuilds[j].ModTime)
+	})
 
 	ctx.Data["Name"] = projectPretty(projectName)
 	ctx.Data["Builds"] = projectBuilds
@@ -27,4 +35,15 @@ func getBuildsProject(ctx *macaron.Context) {
 
 type projectBuild struct {
 	Project, ID string
+	ModTime     time.Time
+}
+
+func modTime(path string) (t time.Time) {
+	if file, err := os.Open(path); err == nil {
+		if fileInfo, err := file.Stat(); err == nil {
+			t = fileInfo.ModTime()
+		}
+	}
+
+	return t
 }
