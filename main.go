01ec1fe5b03a4983ec7ddad01496924440eeddf3
Michael Fellinger <michael.fellinger@xing.com>
2018-07-21 19:16:02 +0200

    Add HOST and PORT flags

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"os/exec"
 	"runtime"
+	"time"
 
 	macaron "gopkg.in/macaron.v1"
 
@@ -19,9 +21,14 @@ var pool *tunny.Pool
 var config struct {
 	GithubUser  string `arg:"--github-user,required,env:GITHUB_USER"`
 	GithubToken string `arg:"--github-token,required,env:GITHUB_TOKEN"`
+	Host        string `arg:"env:HOST"`
+	Port        int    `arg:"env:PORT"`
 }
 
 func main() {
+	config.Host = "0.0.0.0"
+	config.Port = 8080
+
 	arg.MustParse(&config)
 
 	pool = tunny.NewFunc(runtime.NumCPU(), worker)
@@ -33,11 +40,20 @@ func main() {
 	m.Use(macaron.Renderer(macaron.RenderOptions{
 		Layout:     "layout",
 		Extensions: []string{".html"},
+		Funcs: []template.FuncMap{
+			{"FormatTime": func(t time.Time) string {
+				return t.Format(time.RFC1123)
+			}},
+		},
 	}))
 
 	setupRouting(m)
+	m.Run(config.Host, config.Port)
 
-	m.Run(8080)
+	// mux := http.NewServeMux()
+	// mux.Handle("/", m)
+
+	// graceful.Run(config.Host+":"+config.Port, 2*time.Second, mux)
 }
 
 func worker(work interface{}) interface{} {
